1a3bf91ba7e2214574a5b4c1744d80da6857644b
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-01-31 23:47:16 +0100

    integration of top navbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,57 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
 
+import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+
+let openMenu = ref(false)
+console.log(openMenu.value);
 </script>
 
 <template>
 
-    <div class="text-red-500 text-3xl font-bold">Hello Spotify Clone 🎵</div>
+    <div>
+      <div id="TopNav"
+           class="
+            w-[calc(100%-240px)]
+            h-[60px]
+            fixed
+            right-0
+            z-20
+            bg-[#101010]
+            bg-opacity-80
+            flex
+            items-center
+            justify-between
+          ">
+        <div class="flex items-center ml-6">
+          <button type="button" class="rounded-full bg-black p-[1px] cursor-pointer">
+            <ChevronLeft fillColor="#FFFFFF" :size="30" />
+          </button>
+          <button type="button" class="rounded-full bg-black p-[1px] hover:bg-[#] ml-4 cursor-pointer">
+            <ChevronRight fillColor="#FFFFFF" :size="30" />
+          </button>
+        </div>
+        <button @click="openMenu = !openMenu" :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
+                class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer">
+          <div class="flex items-center">
+            <img
+                class="rounded-full"
+                width="27"
+                src="https://yt3.ggpht.com/yti/ANjgQV9IjgYB411P2B7kFxz9krM2pIIbgpeLgvDUxaEMsXR5ibZO=s88-c-k-c0x00ffffff-no-rj"
+            >
+            <div class="text-white text-[14px] ml-1.5 font-semibold">ydev</div>
+            <ChevronDown v-if="!openMenu" @click="openMenu = true" fillColor="#FFFFFF" :size="25" />
+            <ChevronUp v-else @click="openMenu = false" fillColor="#FFFFFF" :size="25" />
+          </div>
+        </button>
+
+      </div>
+    </div>
   </template>
 
 <style scoped>
--- app/src/main.js
+++ app/src/main.js
@@ -6,9 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
