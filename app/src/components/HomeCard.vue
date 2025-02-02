c161eb73bc4a27d48021e2aeb0b302e59b081944
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-02-02 02:13:42 +0100

    feat: main section integration #

---
+++ app/src/components/HomeCard.vue
@@ -0,0 +1,15 @@
+<script>
+import { ref } from 'vue';
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  image: String,
+  title: String,
+  subTitle: String
+})
+const { image, title, subTitle } = toRefs(props)
+
+</script>
+<template>
+<RouterLink to="library"
+</template>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
+<div class="p-8">
+  <button class="text-white text-2xl font-semibold hover:underline"
+          type="button"
 
+  >
+    Podcasts To make you a better developer
+  </button>
+  <div class="py-1.5"></div>
+
+  <div class="flex items-center">
+    <HomeCard image="https://picsum.photos/id/30/300/300" title="Title is here" subTitle="Subtitle is here" />
+  </div>
+</div>
 </template>
